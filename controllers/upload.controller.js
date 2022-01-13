8e8e38129873afa890e7e68a9aecd11f67d09906
owenm <owenmoga1208@gmail.com>
2022-01-13 22:13:25 +0100

    initial front-end

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,12 +1,11 @@
 const UserModel = require('../models/user.model');
 const fs = require('fs');
 const { promisify } = require('util');
-const {uploadErrors} = require("../utils/errors.utils");
 const pipeline = promisify(require('stream').pipeline);
+const {uploadErrors} = require("../utils/errors.utils");
 
 module.exports.uploadProfil = async (req, res) => {
     try {
-        console.log(req.file.detectedMimeType)
         if (req.file.detectedMimeType !== 'image/jpg' && req.file.detectedMimeType !=='image/jpeg' && req.file.detectedMimeType !=='image/png'){
             throw Error("invalid file")
         }
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
 require('dotenv').config({path: ('./config/.env')});
 require('./config/db');
-const {checkUser, requireAuth} = require('./middleware/auth.middleware')
+const {checkUser, requireAuth} = require('./middleware/auth.middleware');
+const cors = require('cors');
+
 const app = express();
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    'preflightContinue': false
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
